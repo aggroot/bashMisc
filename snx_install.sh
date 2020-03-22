598697a2bace37d0d907c9112d8efd864a8f1412
Adrian Galbenus <agg4tech@gmail.com>
2020-03-22 17:07:14 +0200

    Update snx_install.sh

--- snx_install.sh
+++ snx_install.sh
@@ -4,7 +4,7 @@ sudo dpkg --add-architecture i386sudo && sudo apt update
 sudo apt install -y libstdc++5:i386 libx11-6:i386 libpam0g:i386
 
 sudo bash -c "curl -fsSL https://starkers.keybase.pub/snx_install_linux30.sh?dl=1 | bash"
-cat << THIS_EOF >> ~/.snxrc
+cat << THIS_EOF > ~/.snxrc
 server access.axway.net
 username $1
 reauth yes
