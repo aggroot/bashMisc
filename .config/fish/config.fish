51ab3739914f9e1fbad6d1b01b056aa7eb713149
Adrian Galbenus <agg4tech@gmail.com>
2023-09-05 15:24:00 +0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,8 @@
 set -x PATH $HOME/.local/bin $PATH
 set -x LD_LIBRARY_PATH $HOME/.local/lib $LD_LIBRARY_PATH
+
+function sdk
+    bash -c "source '$HOME/.sdkman/bin/sdkman-init.sh'; sdk $argv[1..]"
+end
+
+fish_add_path (find $HOME/.sdkman/candidates/*/current/bin -maxdepth 0)
