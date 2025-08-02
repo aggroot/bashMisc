079435c7a20fc9e7fd7b11304f6ac1585d1ec08a
aggroot <agg4tech@gmail.com>
2025-08-02 12:19:19 +0300

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,29 @@ function sdk
     bash -c "source '$HOME/.sdkman/bin/sdkman-init.sh'; sdk $argv[1..]"
 end
 
-fish_add_path (find $HOME/.sdkman/candidates/*/current/bin -maxdepth 0)
+for dir in $HOME/.sdkman/candidates/*/current/bin
+    if test -d $dir
+        fish_add_path $dir
+    end
+end
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
+set -gx GOPATH $HOME/go; set -gx GOROOT $HOME/.go; set -gx PATH $GOPATH/bin $PATH; # g-install: do NOT edit, see https://github.com/stefanmaric/g
+
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
